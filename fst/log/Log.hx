7f4d3df4498b452fcc19076e7a755c8fb620de0d
Alexander Kuzmenko <alex@stablex.ru>
2013-12-08 19:15:18 +0400

    basic logger

---
+++ fst/log/Log.hx
@@ -0,0 +1,143 @@
+package log;
+
+import haxe.io.Output;
+
+
+/**
+* :TODO:
+* Implement syslog
+*/
+
+
+/**
+* Base logger
+*
+*/
+class Log {
+    /**
+    * Message priorities accroding to RFC 3164 {
+    */
+        /** Emergency: system is unusable */
+        static public inline var PRI_EMERG = 0;
+        /** Alert: action must be taken immediately */
+        static public inline var PRI_ALERT = 1;
+        /** Critical: critical conditions */
+        static public inline var PRI_CRIT = 2;
+        /** Error: error conditions */
+        static public inline var PRI_ERR = 3;
+        /** Warning: warning conditions */
+        static public inline var PRI_WARN = 4;
+        /** Notice: normal but significant condition */
+        static public inline var PRI_NOTICE = 5;
+        /** Informational: informational messages */
+        static public inline var PRI_INFO = 6;
+        /** Debug: debug-level messages */
+        static public inline var PRI_INFO = 7;
+    /**
+    * }
+    */
+
+
+    /** where to send log messages */
+    private var _out : Output;
+
+
+    /**
+    * Constructor
+    *
+    * @param out - where to send messages. Defaults to Sys.stderr() for system targets and to trace() for other targets.
+    */
+    public function new (out:Output = null) : Void {
+        if( out == null ){
+            #if sys
+                this._out = Sys.stderr();
+            #else
+                this._out = new TraceOutput();
+            #end
+        }else{
+            this._out = out;
+        }
+    }//function new()
+
+
+    /**
+    * Log message
+    *
+    */
+    public function log (priority:Int, message:String) : Void {
+        this._out.writeString(DateTools.format(Date.now(), format) +': $message');
+    }//function log()
+
+
+    /**
+    * Log emergency level message
+    *
+    */
+    public function emerge (message:String) : Void {
+        this.log(PRI_EMERGE, message);
+    }//function emerge()
+
+
+    /**
+    * Log alert level message
+    *
+    */
+    public function alert (message) : Void {
+        this.log(PRI_ALERT, message);
+    }//function alert()
+
+
+    /**
+    * Log critical level message
+    *
+    */
+    public function crit (message:String) : Void {
+        this.log(PRI_CRIT, message);
+    }//function crit()
+
+
+    /**
+    * Log error level message
+    *
+    */
+    public function err (message:String) : Void {
+        this.log(PRI_ERR, message);
+    }//function err()
+
+
+    /**
+    * Log warning level message
+    *
+    */
+    public function warn (message:String) : Void {
+        this.log(PRI_WARN, message);
+    }//function warn()
+
+
+    /**
+    * Log notice level message
+    *
+    */
+    public function notice (message:String) : Void {
+        this.log(PRI_NOTICE, message);
+    }//function notice()
+
+
+    /**
+    * Log notice level message
+    *
+    */
+    public function info (message:String) : Void {
+        this.log(PRI_NOTICE, message);
+    }//function info()
+
+
+    /**
+    * Log debug level message
+    *
+    */
+    public function debug (message:String) : Void {
+        this.log(PRI_DEBUG, message);
+    }//function debug()
+
+}//class Log
---
+++ fst/log/TraceOutput.hx
@@ -0,0 +1,21 @@
+package log;
+
+import haxe.io.Output;
+
+
+
+/**
+* Default logger output for non-`sys` targets
+*
+*/
+class TraceOuput extends Output{
+
+    /**
+    * trace string
+    *
+    */
+    override public function writeString (s:String) : Void {
+        trace(s);
+    }//function writeString()
+
+}//class TraceOuput
