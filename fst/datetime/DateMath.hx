c3e6fde7e862898af28dd02f948f3c4bfec3ec1f
Alexander Kuzmenko <alex@stablex.ru>
2014-01-14 18:01:16 +0400

    datetime math

---
+++ fst/datetime/DateMath.hx
@@ -0,0 +1,25 @@
+package fst.datetime;
+
+
+/**
+* Arithmetics with dates and time
+*
+*/
+class DateMath {
+    /** amount of seconds in one day */
+    static public inline var SECONDS_IN_DAY = 86400;
+
+
+    /**
+    * Amount of days passed since date1 to date2
+    * E.g.:
+    *   if date1 = 2013-12-05, date2 = 2013-12-05 returns 0
+    *   if date1 = 2013-12-05, date2 = 2013-12-07 returns 2
+    *   if date1 = 2013-12-07, date2 = 2013-12-05 returns -2
+    */
+    static public inline function deltaDays (date1:Date, date2:Date) : Int {
+        return Std.int(date2.getTime() / SECONDS_IN_DAY) - Std.int(date1.getTime() / SECONDS_IN_DAY);
+    }//function deltaDays()
+
+
+}//class DateMath
