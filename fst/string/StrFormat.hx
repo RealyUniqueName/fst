c9b94b0679430809a3e9bfea0f7d22d0c7291ed1
Alexander Kuzmenko <alex@stablex.ru>
2014-02-26 16:31:22 +0400

    fix StrFormat.number()

--- fst/string/StrFormat.hx
+++ fst/string/StrFormat.hx
@@ -18,10 +18,10 @@ class StrFormat {
     * @param thousandsSeparator
     */
     static public function number (num:Float, decimals:Int = 0, decPoint:String = '.', thousandsSeparator:String = ',') : String {
-        var str : String = '';
         var int : String = Std.string( Std.int(num) );
+        var str : String = int.charAt(int.length - 1);
 
-        for(i in 0...int.length){
+        for(i in 1...int.length){
             if( i % 3 == 0 ){
                 str = thousandsSeparator + str;
             }
